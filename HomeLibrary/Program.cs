0d0e7e013ce259a1bcee58ef45c55b3bc48cfa5b
sar0868 <safarov-ar@yandex.ru>
2025-08-10 18:47:52 +0300

    add title, author, year, ISBN

--- HomeLibrary/Program.cs
+++ HomeLibrary/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string title;
+string author;
+int year;
+string ISBN;
+
+Console.Write("Название: ");
+title = Console.ReadLine();
+
+Console.Write("Автор: ");
+author = Console.ReadLine();
+
+Console.Write("Год издания: ");
+var year_str = Console.ReadLine();
+year = int.Parse(year_str);
+
+Console.Write("ISBN: ");
+ISBN = Console.ReadLine();
+
+Console.WriteLine($"Название {title}");
+Console.WriteLine($"Автор {author}");
+Console.WriteLine($"Год издания {year}");
+Console.WriteLine($"ISBN {ISBN}");
