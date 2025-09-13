07cd2e96fe74320f283c2b85a052e8bb21b84f11
sar0868 <safarov-ar@yandex.ru>
2025-09-13 16:27:16 +0300

    module 8

--- HomeLibrary/Book.cs
+++ HomeLibrary/Book.cs
@@ -1,29 +1,3 @@
 namespace HomeLibrary;
 
-// public class Book
-// {
-//     public string Title { get; set; }
-//     public string Author { get; set; }
-//     public string Year { get; set; }
-//     public string ISBN { get; set; }
-//     public string Comment { get; set; }
-//     public bool IsRead { get; set; }
-
-
-//     public Book(string title, string author, string year, string ISBN)
-//     {
-//         Title = title;
-//         Author = author;
-//         Year = year;
-//         this.ISBN = ISBN;
-//         IsRead = false;
-//         Comment = "";
-//     }
-
-//     public override string ToString()
-//     {
-//         return $"Название: {Title}, Автор: {Author}, Год издания: {Year}, ISBN: {ISBN}";
-//     }
-// }
-
 public record Book(string? Title, string? Author, string? Year, string? ISBN, string? Comment, bool IsRead);
